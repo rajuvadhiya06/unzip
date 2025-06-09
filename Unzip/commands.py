85e2587637176d1b8041b2f0741b4fdbd30f9272
rajuvadhiya06 <vadhiyaraju709@gmail.com>
2025-06-09 16:56:52 +0530

    Add files via upload

---
+++ Unzip/commands.py
@@ -0,0 +1,62 @@
+# ©️ LISA-KOREA | @LISA_FAN_LK | NT_BOT_CHANNEL | LISA-KOREA/UnZip-Bot
+
+# [⚠️ Do not change this repo link ⚠️] :- https://github.com/LISA-KOREA/UnZip-Bot
+
+
+
+from pyrogram import Client, filters
+from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
+
+active_tasks = {}
+
+
+@Client.on_message(filters.command("start"))
+async def start(client, message):
+    reply_markup = InlineKeyboardMarkup(
+    [
+        [
+            InlineKeyboardButton("📍 Update Channel", url="https://t.me/NT_BOT_CHANNEL"),
+        ],
+        [
+            InlineKeyboardButton("👥 Support Group", url="https://t.me/NT_BOTS_SUPPORT"),
+            InlineKeyboardButton("👩‍💻 Developer", url="https://t.me/LISA_FAN_LK"),
+        ] 
+   ]
+  )
+    start_message = (
+        "Hello!\n\n"
+        "Send me a ZIP file, and I'll extract it for you. I also support .RAR, .7z, .tar, and more."
+    )
+    await message.reply(start_message, reply_markup=reply_markup)
+
+
+# Callback query handler
+@Client.on_callback_query(filters.regex("cancel"))
+async def cancel(client, callback_query):
+    await callback_query.message.delete()
+
+
+@Client.on_message(filters.command("help"))
+async def help_command(client, message):
+    help_message = (
+        "Here are the commands you can use:\n\n"
+        "/start - Start the bot and get the welcome message\n"
+        "/help - Get help on how to use the bot\n\n"
+        "To unzip a file, simply send me a ZIP file and I will extract its contents and send them back to you.\n\n"
+        "©️ Channel : @NT_BOT_CHANNEL"
+    )
+    await message.reply(help_message)
+
+
+
+@Client.on_callback_query(filters.regex("cancel_unzip"))
+async def cancel_callback(client, callback_query):
+    user_id = callback_query.from_user.id
+
+    if user_id in active_tasks:
+        task = active_tasks[user_id]
+        task.cancel()
+        await callback_query.answer("⛔ Unzipping has been cancelled.", show_alert=True)
+    else:
+        await callback_query.answer("⚠️ No ongoing unzip operation.", show_alert=True)
+
